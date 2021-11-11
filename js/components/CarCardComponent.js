442b1c63d74898596d13d946ad9e4122d3bdaf9e
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-11 19:45:10 +0200

    8: kortelės atvaizdavimas

--- js/components/CarCardComponent.js
+++ js/components/CarCardComponent.js
@@ -1,4 +1,6 @@
 class CarCardComponent {
+  static USD_EUR = 0.87;
+
   constructor(props) {
     this.props = props;
 
@@ -6,10 +8,32 @@ class CarCardComponent {
   }
 
   init = () => {
+    const { brand, model, year, fuelType, price, imgSrc } = this.props;
+    const { amount, currency } = price;
+
+    const finalPrice = currency === '$' ? amount * CarCardComponent.USD_EUR : amount;
+    const formatedPrice = Math.round(100 * finalPrice) / 100 + ' €';
+
     this.htmlElement = document.createElement('article');
-    this.htmlElement.className = 'card p-3 shadow';
+    this.htmlElement.className = 'card shadow';
     this.htmlElement.innerHTML = `
-    <h2 class="h5">Kortelė</h2>
-    `;
+    <img src="${imgSrc}" class="card-img-top"/ height="200px" style="object-fit: cover">
+    <div class="card-body">
+      <h2 class="h5">${brand} ${model}</h2>
+      <ul>
+        <li>
+          <span>year</span>:
+          <strong>${year}</strong>
+        </li>
+        <li>
+          <span>fuel type</span>:
+          <strong>${fuelType}</strong>
+        </li>
+        <li>
+          <span>price</span>:
+          <strong>${formatedPrice}</strong>
+        </li>
+      </ul>
+    </div>`;
   }
 }
