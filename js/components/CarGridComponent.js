e4d1985610278f5e5f4f2b643eef15ac72588edb
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-11 19:09:51 +0200

    6: data fetch in component

--- js/components/CarGridComponent.js
+++ js/components/CarGridComponent.js
@@ -1,10 +1,35 @@
 class CarGridComponent {
   constructor() {
+    this.state = {
+      loading: false,
+      cars: []
+    }
     this.init();
   }
 
+  fetchCars = () => API.fetchCars(this.saveCars, alert);
+
+  saveCars = (cars) => {
+    this.state.cars = cars;
+    this.state.loading = false;
+
+    this.render();
+  }
+
   init = () => {
+    this.state.loading = true;
+    this.fetchCars();
     this.htmlElement = document.createElement('div');
-    this.htmlElement.innerHTML = 'Aš esu CarGridComponent';
+
+    this.render();
+  }
+
+  render = () => {
+    const { loading, cars } = this.state;
+    if (loading) {
+      this.htmlElement.innerHTML = 'siunčiama...';
+    } else {
+      this.htmlElement.innerHTML = 'parsiųsta...';
+    }
   }
 }
