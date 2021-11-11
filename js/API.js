37ee513309f414fc741ccdac878f7c7e511f0358
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-11 19:26:21 +0200

    7: kortelių atvaizdavimas pagal duomenis, animacija

--- js/API.js
+++ js/API.js
@@ -2,10 +2,12 @@ const baseURL = 'http://localhost:3000';
 
 class API {
   static fetchCars = (success, failure) => {
-    fetch(`${baseURL}/cars`)
+    setTimeout(() => {
+      fetch(`${baseURL}/cars`)
       .then(res => res.json())
       .then(success)
       .catch(failure)
+    }, 1000);
   }
   static deleteCar = (id, success, failure) => {
     fetch(`${baseURL}/cars/${id}`, { method: 'DELETE' })
--- js/components/CarGridComponent.js
+++ js/components/CarGridComponent.js
@@ -20,16 +20,31 @@ class CarGridComponent {
     this.state.loading = true;
     this.fetchCars();
     this.htmlElement = document.createElement('div');
+    this.htmlElement.className = 'row g-3';
 
     this.render();
+  }
+
+  wrapInColumn = (element) => {
+    const column = document.createElement('div');
+    column.className = 'col-12 col-sm-6 col-lg-4 col-xl-3';
+    column.appendChild(element);
+    return column;
   }
 
   render = () => {
     const { loading, cars } = this.state;
     if (loading) {
-      this.htmlElement.innerHTML = 'siunčiama...';
+      this.htmlElement.innerHTML = `<div class="text-center"><img src="assets/loading.gif"/></div>`;
+    } else if (cars.length > 0) {
+      this.htmlElement.innerHTML = '';
+      const carElements = cars
+        .map(x => new CarCardComponent(x))
+        .map(x => x.htmlElement)
+        .map(this.wrapInColumn);
+      this.htmlElement.append(...carElements)
     } else {
-      this.htmlElement.innerHTML = 'parsiųsta...';
+      this.htmlElement.innerHTML = `<h2>Šiuo metu mašinų nėra</h2>`;
     }
   }
 }
